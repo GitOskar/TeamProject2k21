38c0fcddadded7020640bedba79d31da13dc1c46
Oskar <67687200+GitOskar@users.noreply.github.com>
2021-03-24 19:18:54 +0100

    Everything done

---
+++ backend/scholarship-revaluation/src/main/java/scholarshiprevaluation/ScholarshipRevaluationApplication.java
@@ -0,0 +1,13 @@
+package scholarshiprevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarshipRevaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarshipRevaluationApplication.class, args);
+	}
+
+}
